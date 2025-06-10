f4fcd40cc77107200026584426569aae7e69f232
Danila Ulyanov <9megaton6@gmail.com>
2025-06-10 19:27:36 +0300

    webgpu: add transfer function features

--- devices/webgpu/webgpu_input_process.cpp
+++ devices/webgpu/webgpu_input_process.cpp
@@ -13,21 +13,122 @@ OIDN_NAMESPACE_BEGIN
   static const char* kWGSL = R"wgsl(
   struct Image { data: array<f32>; };
   struct Tensor { data: array<f32>; };
-  struct Size { h:u32, w:u32, c:u32 }; 
+  struct Params {
+    h:u32; w:u32; c:u32; tfType:u32;
+    inputScale:f32; normScale:f32; rcpNormScale:f32;
+    hdr:u32; snorm:u32; hasAlbedo:u32; hasNormal:u32;
+  };
 
-  @group(0) @binding(0) var<storage, read>  src : Image;
-  @group(0) @binding(1) var<storage, read_write> dst : Tensor;
-  @group(0) @binding(2) var<uniform> size : Size;
+  @group(0) @binding(0) var<storage, read>  inputImg : Image;
+  @group(0) @binding(1) var<storage, read>  albedoImg : Image;
+  @group(0) @binding(2) var<storage, read>  normalImg : Image;
+  @group(0) @binding(3) var<storage, read_write> dst : Tensor;
+  @group(0) @binding(4) var<uniform> params : Params;
+
+  fn srgb_forward(y:f32) -> f32 {
+    let a = 12.92;
+    let b = 1.055;
+    let c = 1.0/2.4;
+    let d = -0.055;
+    let y0 = 0.0031308;
+    if (y <= y0) { return a*y; }
+    return b*pow(y, c) + d;
+  }
+
+  fn pu_forward(y:f32) -> f32 {
+    let A=1.41283765e+03;
+    let B=1.64593172e+00;
+    let C=4.31384981e-01;
+    let D=-2.94139609e-03;
+    let E=1.92653254e-01;
+    let F=6.26026094e-03;
+    let G=9.98620152e-01;
+    let Y0=1.57945760e-06;
+    let Y1=3.22087631e-02;
+    if (y <= Y0) { return A*y; }
+    if (y <= Y1) { return B*pow(y,C)+D; }
+    return E*log(y+F)+G;
+  }
+
+  fn tf_forward(y: vec3<f32>) -> vec3<f32> {
+    switch(params.tfType) {
+      default { return y; }
+      case 1u {
+        return vec3<f32>(srgb_forward(y.x), srgb_forward(y.y), srgb_forward(y.z));
+      }
+      case 2u {
+        let r = vec3<f32>(pu_forward(y.x), pu_forward(y.y), pu_forward(y.z));
+        return r * params.normScale;
+      }
+      case 3u {
+        return log(y + vec3<f32>(1.0)) * params.normScale;
+      }
+    }
+  }
+
+  fn sanitize(v: vec3<f32>, minV:f32, maxV:f32) -> vec3<f32> {
+    var r = v;
+    if (isNan(r.x)) { r.x = 0.0; }
+    if (isNan(r.y)) { r.y = 0.0; }
+    if (isNan(r.z)) { r.z = 0.0; }
+    r = clamp(r, vec3<f32>(minV), vec3<f32>(maxV));
+    return r;
+  }
 
   @compute @workgroup_size(8,8,1)
   fn main(@builtin(global_invocation_id) gid: vec3<u32>) {
     let h = gid.y;
     let w = gid.x;
-    if (h >= size.h || w >= size.w) { return; }
-    for (var i:u32 = 0u; i < size.c; i = i + 1u) {
-      let srcIdx = ((h*size.w + w)*3u) + i;
-      let dstIdx = ((h*size.w + w)*size.c) + i;
-      dst.data[dstIdx] = src.data[srcIdx];
+    if (h >= params.h || w >= params.w) { return; }
+
+    let imgIdx = ((h*params.w + w)*3u);
+    var value = vec3<f32>(
+        inputImg.data[imgIdx],
+        inputImg.data[imgIdx+1u],
+        inputImg.data[imgIdx+2u]);
+    value = value * params.inputScale;
+    let minV = select(0.0, -1.0, params.snorm != 0u);
+    let maxV = select(1.0, 3.4028235e38, params.hdr != 0u);
+    value = sanitize(value, minV, maxV);
+    if (params.snorm != 0u) {
+      value = value * 0.5 + vec3<f32>(0.5);
+    }
+    value = tf_forward(value);
+
+    var dstIdx = ((h*params.w + w)*params.c);
+    dst.data[dstIdx] = value.x;
+    if (params.c > 1u) { dst.data[dstIdx+1u] = value.y; }
+    if (params.c > 2u) { dst.data[dstIdx+2u] = value.z; }
+    var c = 3u;
+
+    if (params.hasAlbedo != 0u) {
+      var alb = vec3<f32>(
+          albedoImg.data[imgIdx],
+          albedoImg.data[imgIdx+1u],
+          albedoImg.data[imgIdx+2u]);
+      alb = sanitize(alb, 0.0, 1.0);
+      if (c < params.c) { dst.data[dstIdx+c] = alb.x; }
+      if (c+1u < params.c) { dst.data[dstIdx+c+1u] = alb.y; }
+      if (c+2u < params.c) { dst.data[dstIdx+c+2u] = alb.z; }
+      c = c + 3u;
+
+      if (params.hasNormal != 0u) {
+        var nrm = vec3<f32>(
+            normalImg.data[imgIdx],
+            normalImg.data[imgIdx+1u],
+            normalImg.data[imgIdx+2u]);
+        nrm = sanitize(nrm, -1.0, 1.0);
+        nrm = nrm * 0.5 + vec3<f32>(0.5);
+        if (c < params.c) { dst.data[dstIdx+c] = nrm.x; }
+        if (c+1u < params.c) { dst.data[dstIdx+c+1u] = nrm.y; }
+        if (c+2u < params.c) { dst.data[dstIdx+c+2u] = nrm.z; }
+        c = c + 3u;
+      }
+    }
+
+    while (c < params.c) {
+      dst.data[dstIdx+c] = 0.0;
+      c = c + 1u;
     }
   }
   )wgsl";
@@ -36,9 +137,15 @@ OIDN_NAMESPACE_BEGIN
   {
     check();
 
-    if (!color || albedo || normal)
-      throw std::logic_error("unsupported input process configuration");
-    if (color->getFormat() != Format::Float3)
+    Image* mainSrc = getMainSrc();
+    if (!mainSrc || mainSrc->getFormat() != Format::Float3)
+      throw std::invalid_argument("unsupported image format");
+
+    bool hasAlbedo = color && albedo;
+    bool hasNormal = color && normal;
+    if (hasAlbedo && albedo->getFormat() != Format::Float3)
+      throw std::invalid_argument("unsupported image format");
+    if (hasNormal && normal->getFormat() != Format::Float3)
       throw std::invalid_argument("unsupported image format");
 
     auto* dev = static_cast<WebGPUDevice*>(engine->getDevice());
@@ -50,16 +157,29 @@ OIDN_NAMESPACE_BEGIN
     size_t srcSize = size_t(H)*W*3*sizeof(float);
     WGPUBuffer srcBuf = dev->createBuffer(srcSize,
                     WGPUBufferUsage_Storage | WGPUBufferUsage_CopyDst,
-                    color->getPtr());
+                    mainSrc->getPtr());
+    WGPUBuffer albedoBuf = hasAlbedo ?
+        dev->createBuffer(srcSize, WGPUBufferUsage_Storage | WGPUBufferUsage_CopyDst, albedo->getPtr()) :
+        dev->createBuffer(4, WGPUBufferUsage_Storage);
+    WGPUBuffer normalBuf = hasNormal ?
+        dev->createBuffer(srcSize, WGPUBufferUsage_Storage | WGPUBufferUsage_CopyDst, normal->getPtr()) :
+        dev->createBuffer(4, WGPUBufferUsage_Storage);
 
     auto* dbuf = dynamic_cast<WebGPUBuffer*>(dst->getBuffer());
     if (!dbuf)
       throw std::invalid_argument("destination tensor not on WebGPU device");
 
-    struct Size { uint32_t h,w,c; } size = { (uint32_t)H, (uint32_t)W, (uint32_t)C };
-    WGPUBuffer sizeBuf = dev->createBuffer(sizeof(Size),
-                        WGPUBufferUsage_Uniform | WGPUBufferUsage_CopyDst,
-                        &size);
+    struct Params
+    {
+      uint32_t h,w,c,tfType;
+      float inputScale,normScale,rcpNormScale;
+      uint32_t hdr,snorm,hasAlbedo,hasNormal;
+    } params = { (uint32_t)H,(uint32_t)W,(uint32_t)C,(uint32_t)transferFunc->getType(),
+                 transferFunc->getInputScale(), transferFunc->normScale, transferFunc->rcpNormScale,
+                 hdr ? 1u : 0u, snorm ? 1u : 0u, hasAlbedo ? 1u : 0u, hasNormal ? 1u : 0u };
+    WGPUBuffer paramsBuf = dev->createBuffer(sizeof(Params),
+                           WGPUBufferUsage_Uniform | WGPUBufferUsage_CopyDst,
+                           &params);
 
     WGPUShaderSourceWGSL source{};
     source.chain.next = nullptr;
@@ -70,16 +190,20 @@ OIDN_NAMESPACE_BEGIN
     smDesc.label = { nullptr, 0 };
     WGPUShaderModule shader = wgpuDeviceCreateShaderModule(dev->device, &smDesc);
 
-    WGPUBindGroupLayoutEntry entries[3] = {};
+    WGPUBindGroupLayoutEntry entries[5] = {};
     entries[0].binding = 0; entries[0].visibility = WGPUShaderStage_Compute;
     entries[0].buffer.type = WGPUBufferBindingType_ReadOnlyStorage;
     entries[1].binding = 1; entries[1].visibility = WGPUShaderStage_Compute;
-    entries[1].buffer.type = WGPUBufferBindingType_Storage;
+    entries[1].buffer.type = WGPUBufferBindingType_ReadOnlyStorage;
     entries[2].binding = 2; entries[2].visibility = WGPUShaderStage_Compute;
-    entries[2].buffer.type = WGPUBufferBindingType_Uniform;
+    entries[2].buffer.type = WGPUBufferBindingType_ReadOnlyStorage;
+    entries[3].binding = 3; entries[3].visibility = WGPUShaderStage_Compute;
+    entries[3].buffer.type = WGPUBufferBindingType_Storage;
+    entries[4].binding = 4; entries[4].visibility = WGPUShaderStage_Compute;
+    entries[4].buffer.type = WGPUBufferBindingType_Uniform;
 
     WGPUBindGroupLayoutDescriptor bglDesc{};
-    bglDesc.entryCount = 3;
+    bglDesc.entryCount = 5;
     bglDesc.entries = entries;
     WGPUBindGroupLayout bgl = wgpuDeviceCreateBindGroupLayout(dev->device, &bglDesc);
 
@@ -99,13 +223,15 @@ OIDN_NAMESPACE_BEGIN
     cpDesc.label = { nullptr, 0 };
     WGPUComputePipeline pipeline = wgpuDeviceCreateComputePipeline(dev->device, &cpDesc);
 
-    WGPUBindGroupEntry bgEntries[3] = {};
+    WGPUBindGroupEntry bgEntries[5] = {};
     bgEntries[0].binding = 0; bgEntries[0].buffer = srcBuf; bgEntries[0].size = srcSize;
-    bgEntries[1].binding = 1; bgEntries[1].buffer = dbuf->getWGPUBuffer(); bgEntries[1].offset = dst->getByteOffset(); bgEntries[1].size = size_t(C)*H*W*sizeof(float);
-    bgEntries[2].binding = 2; bgEntries[2].buffer = sizeBuf; bgEntries[2].size = sizeof(Size);
+    bgEntries[1].binding = 1; bgEntries[1].buffer = albedoBuf; bgEntries[1].size = hasAlbedo ? srcSize : 4;
+    bgEntries[2].binding = 2; bgEntries[2].buffer = normalBuf; bgEntries[2].size = hasNormal ? srcSize : 4;
+    bgEntries[3].binding = 3; bgEntries[3].buffer = dbuf->getWGPUBuffer(); bgEntries[3].offset = dst->getByteOffset(); bgEntries[3].size = size_t(C)*H*W*sizeof(float);
+    bgEntries[4].binding = 4; bgEntries[4].buffer = paramsBuf; bgEntries[4].size = sizeof(Params);
     WGPUBindGroupDescriptor bgDesc{};
     bgDesc.layout = bgl;
-    bgDesc.entryCount = 3;
+    bgDesc.entryCount = 5;
     bgDesc.entries = bgEntries;
     WGPUBindGroup bg = wgpuDeviceCreateBindGroup(dev->device, &bgDesc);
 
@@ -127,8 +253,10 @@ OIDN_NAMESPACE_BEGIN
     wgpuBindGroupLayoutRelease(bgl);
     wgpuComputePipelineRelease(pipeline);
     wgpuShaderModuleRelease(shader);
-    wgpuBufferRelease(sizeBuf);
+    wgpuBufferRelease(paramsBuf);
     wgpuBufferRelease(srcBuf);
+    wgpuBufferRelease(albedoBuf);
+    wgpuBufferRelease(normalBuf);
   }
 
 OIDN_NAMESPACE_END
--- tests/test_webgpu_input_process.cpp
+++ tests/test_webgpu_input_process.cpp
@@ -67,3 +67,73 @@ TEST(WebGPU, InputProcess)
     ASSERT_NEAR(out[i], ref[i], 1e-6f);
 }
 
+TEST(WebGPU, InputProcessAdvanced)
+{
+  if (!isWebGPUDeviceSupported())
+    GTEST_SKIP();
+
+  auto dev = newWebGPUDevice();
+  dev.commit();
+  WebGPUEngine* eng = getEngine(dev);
+
+  const uint32_t H=2, W=2;
+  float color[H*W*3];
+  float albedo[H*W*3];
+  float normal[H*W*3];
+  for(size_t i=0;i<H*W*3;++i)
+  {
+    color[i]  = -0.5f + float(i)*0.5f;
+    albedo[i] = float(i)*0.1f;
+    normal[i] = -1.f + float(i)*0.2f;
+  }
+
+  float ref[9*H*W];
+
+  if (isCPUDeviceSupported())
+  {
+    auto cpuDev = newDevice(DeviceType::CPU);
+    cpuDev.commit();
+    auto cpuImpl = static_cast<CPUDevice*>(reinterpret_cast<Device*>(cpuDev.getHandle()));
+    CPUEngine* cpuEng = static_cast<CPUEngine*>(cpuImpl->getEngine());
+    TensorDims dims{9,int(H),int(W)};
+    auto tf = std::make_shared<TransferFunction>(TransferFunction::Type::SRGB);
+    auto proc = cpuEng->newInputProcess({dims, tf, true, true});
+    auto colorImg = makeRef<Image>(color, Format::Float3, W, H, 0, sizeof(float)*3, sizeof(float)*3*W);
+    auto albedoImg = makeRef<Image>(albedo, Format::Float3, W, H, 0, sizeof(float)*3, sizeof(float)*3*W);
+    auto normalImg = makeRef<Image>(normal, Format::Float3, W, H, 0, sizeof(float)*3, sizeof(float)*3*W);
+    proc->setSrc(colorImg, albedoImg, normalImg);
+    auto dstDesc = proc->getDstDesc();
+    auto dstTensor = makeRef<HostTensor>(dstDesc);
+    proc->setDst(dstTensor);
+    proc->submit(nullptr);
+    cpuDev.sync();
+    std::memcpy(ref, dstTensor->getPtr(), sizeof(ref));
+  }
+  else
+  {
+    for(size_t i=0;i<9*H*W;++i) ref[i]=0.f; // not used
+  }
+
+  TensorDims dims{9,int(H),int(W)};
+  auto tf = std::make_shared<TransferFunction>(TransferFunction::Type::SRGB);
+  auto proc = eng->newInputProcess({dims, tf, true, true});
+  auto colorImg = makeRef<Image>(color, Format::Float3, W, H, 0, sizeof(float)*3, sizeof(float)*3*W);
+  auto albedoImg = makeRef<Image>(albedo, Format::Float3, W, H, 0, sizeof(float)*3, sizeof(float)*3*W);
+  auto normalImg = makeRef<Image>(normal, Format::Float3, W, H, 0, sizeof(float)*3, sizeof(float)*3*W);
+  proc->setSrc(colorImg, albedoImg, normalImg);
+  auto dstDescGPU = proc->getDstDesc();
+  auto buf = dev.newBuffer(dstDescGPU.getByteSize());
+  auto dstTensorGPU = eng->Engine::newTensor(Ref<Buffer>(reinterpret_cast<Buffer*>(buf.getHandle())), dstDescGPU);
+  proc->setDst(dstTensorGPU);
+  proc->submit(nullptr);
+  dev.sync();
+  float out[9*H*W];
+  buf.read(0, sizeof(out), out);
+
+  if (isCPUDeviceSupported())
+  {
+    for(size_t i=0;i<9*H*W;++i)
+      ASSERT_NEAR(out[i], ref[i], 1e-4f);
+  }
+}
+
